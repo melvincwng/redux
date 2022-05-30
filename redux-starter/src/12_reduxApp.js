bb028a4ef428a1fe522bd6dbcc0a5ef573659a7b
melvincwng <melvinng93@outlook.com>
2022-05-30 16:34:06 +0800

    Add L12_reduxApp.js

---
+++ redux-starter/src/12_reduxApp.js
@@ -0,0 +1,46 @@
+/**
+ * Steps in redux
+ * 1) Design the store
+ * 2) Define the actions
+ * 3) Create a reducer (reducers take an action and return updated state)
+ * 4) Set up the store
+ */
+
+/**
+ * Step 1 (Design the store)
+ * What do we want the store to contain? We want it to contain various application states.
+ * For our bug tracking application, we want it to for example store a list of bugs
+ * Therefore the store can be something like an array of bug objects:
+ * [
+ *    { id: 1, description: "some description", resolved: false },
+ *    {...},
+ *    {...}
+ * ]
+ * But in reality, your redux store is more complex than this...
+ */
+
+/**
+ * Step 2 (Define the actions)
+ * Actions that can be used in this bug tracking app can be like a) Add a bug, b) Delete a bug, or c) Mark a bug as resolved
+ * Actions are just plain JS objects that describe what just happened.
+ * E.g. {
+ *    type: "bugAdded",
+ *    description: "..."
+ * }
+ * Take note, since redux was inspired by Flux (Facebook's creation), you may also see certain action objects like this
+ * E.g. {
+ *    type: "bugAdded",
+ *    payload: {
+ *      id: "..."
+ *      description: "..."
+ *  }
+ * }
+ * The payload contains the minimum amt of information about the action
+ */
+
+/**
+ * Step 3 (Create a reducer)
+ * A reducer essentially is a function containing two parameters (current state and action)
+ * It's job is to return the new state based on this action
+ * Refer to the file reducer.js for more info
+ */
---
+++ redux-starter/src/reducer.js
@@ -0,0 +1,27 @@
+// Recall earlier our state is an array of bug objects [ {id: 1, description: "some description", resolved: false}, {...}, {...}]
+// When we start the app, the store is initially undefined --> meaning state is undefined
+// Hence, at the start, we are passing in undefined as the state argument in the reducer function
+// We don't want it to return undefined... hence we can set a default state which in this case, is an empty array []
+
+let lastId = 0;
+
+function reducer(state = [], action) {
+  if (action.type === "bugAdded")
+    // returning an updated state containing the previous state's bug objects AND a new bug object
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      },
+    ];
+  else if (action.type === "bugRemoved")
+    // return the updated state containing the removed bug
+    return state.filter((bug) => bug.id !== action.payload.id);
+
+  // If we dispatch an action that doesn't exist, we dont want our system to go haywire, hence just return the current state
+  return state;
+}
+
+// Can use switch statements as well in the reducer function
